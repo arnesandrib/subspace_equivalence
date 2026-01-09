9d31352dad069d92fbf3dff10d0379ae9a9b767c
Arne Sandrib <asandrib@gmail.com>
2026-01-09 18:43:04 +0100

    fix name of init

---
+++ compute_sdu/__init__.py
@@ -0,0 +1,6 @@
+# compute_sdu/compute_SDU/__init__.py
+__version__ = "0.1.0"
+
+from .compute_SDU import computeSDU,compute_DDT
+
+__all__ = [ "computeSDU","compute_DDT" ]
--- compute_sdu/init.py
+++
@@ -1,6 +0,0 @@
-# compute_sdu/compute_SDU/__init__.py
-__version__ = "0.1.0"
-
-from .compute_SDU import computeSDU
-
-__all__ = ["compute_sdu"]
